646ad259fbcaadc40cd53d97b053f5efac83c8e2
JayNguyen168 <139840147+JayNguyen168@users.noreply.github.com>
2023-12-16 15:18:51 +0700

    Update reading.h

--- STM32/Core/Inc/reading.h
+++ STM32/Core/Inc/reading.h
@@ -8,6 +8,10 @@
 #ifndef INC_READING_H_
 #define INC_READING_H_
 
+#define N0_OF_BUTTONS 	4
 
+void button_reading (void) ;
+unsigned char is_button_pressed (unsigned char index);
+unsigned char is_button_pressed_1s (unsigned char index);
 
 #endif /* INC_READING_H_ */
