3f392bec3056f7c78f489f4654de73fa07c76665
JayNguyen168 <139840147+JayNguyen168@users.noreply.github.com>
2023-12-16 15:18:25 +0700

    Update processing.h

--- STM32/Core/Inc/processing.h
+++ STM32/Core/Inc/processing.h
@@ -8,6 +8,10 @@
 #ifndef INC_PROCESSING_H_
 #define INC_PROCESSING_H_
 
+#include "reading.h"
 
+extern int is_press_flag[N0_OF_BUTTONS];
+
+void fsm_for_input_processing(void);
 
 #endif /* INC_PROCESSING_H_ */
